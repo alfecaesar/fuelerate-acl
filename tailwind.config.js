40471b20325719ac9641033efef09ce46615259f
Alfe Caesar Lagas <alfecaesar@gmail.com>
2026-09-03 02:56:08 +0800

    add tailwind support

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  prefix: 'tw-',
+
+  content: [
+    './layout/**/*.liquid',
+    './sections/**/*.liquid',
+    './snippets/**/*.liquid',
+    './templates/**/*.liquid',
+    './blocks/**/*.liquid',
+  ],
+
+  theme: {
+    extend: {},
+  },
+
+  plugins: [],
+}
