fae4bdacc123354ce9b1f33df15d9c0e732aefe5
Alfe Caesar Lagas <alfecaesar@gmail.com>
2026-09-05 20:30:50 +0800

    new updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   prefix: 'tw-',
-
+  corePlugins: {
+    preflight: false, // turns off the reset that was messing with your layout
+  },
   content: [
     './layout/**/*.liquid',
     './sections/**/*.liquid',
